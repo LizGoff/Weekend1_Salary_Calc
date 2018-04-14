618af55de74615b2961d4720fe55c9a0544ec330
Liz Goff <livingearthcreations@gmail.com>
2018-04-13 21:17:48 -0500

    functioning counter

--- script.js
+++ script.js
@@ -1,18 +1,35 @@
 console.log("JS");
 
+let clickEmployeeName = 0;
+
 $(document).ready(onReady);
 console.log('Sheet ready');
 
 function onReady() {
     console.log("See sheet");
+    $('button').on("click", clickHandler);
+    displayEmployeeName();
 
-    $("button").on("click", clickHandler);
-  //  newFirstName();
-}
+    function clickHandler() {
+        console.log('was clicked!!!!!!!');
+        clickEmployeeName++;
+        console.log(clickEmployeeName);
+    }
+    //     let click = $("#addNewEmployee");
+    //     console.log($("#addNewEmployee"));
+    //     console.log(click);
+        displayEmployeeName();
+    //     newFirstName();
+    // }
 
-function clickHandler() {
- //   let click = "#newFirstName";
-    console.log("This has been clicked!");
-  //  console.log(click);
-  //  newFirstName();
+     function displayEmployeeName() {
+        let newNameFirst = "#newFirstName";
+        $("#totalMonthly").text(clickEmployeeName);
+    }
+
+    // function addNewInput() {
+    //     $("#addNewEmployee").append("<tr>" + "<td>" + $("#newFirstName").val() + "<td>" + "<td>" + $("#newLastName").val() + "<td>" + "<td>" + $("#newId").val() + "<td>" + "<td>" + $("#newJobTitle").val() + "<td>" + "<td>" + $("#newAnnualSalary").val() + "<td>" + "<tr>");
+    //     console.log($("#addNewEmployee"));
+    // }
+
 }
