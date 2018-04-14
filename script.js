0734a7ff71831b623eea400cb0057ca10c1f4538
Liz Goff <livingearthcreations@gmail.com>
2018-04-13 20:00:12 -0500

    button working

--- script.js
+++ script.js
@@ -5,4 +5,14 @@ console.log('Sheet ready');
 
 function onReady() {
     console.log("See sheet");
+
+    $("button").on("click", clickHandler);
+  //  newFirstName();
+}
+
+function clickHandler() {
+ //   let click = "#newFirstName";
+    console.log("This has been clicked!");
+  //  console.log(click);
+  //  newFirstName();
 }
