0f8639b612ff79aaff516fb0bc0d985ef889d0eb
Liz Goff <livingearthcreations@gmail.com>
2018-04-14 20:33:03 -0500

    some css styling

--- script.js
+++ script.js
@@ -9,12 +9,12 @@ function onReady() {
  //   console.log("See sheet");
     $('button').on("click", clickHandler);
     addEmployee();
+    //totalBudget();
 
     function clickHandler() {
-       console.log('was clicked');
+    console.log('was clicked');
     addEmployee();
-    //displayTotalMonthly();
-    //     "totalMonthly" ?
+    //totalBudget();
     }
 
     //function displayTotalMonthly() {
@@ -24,10 +24,17 @@ function onReady() {
     function addEmployee() {
     $("#addNewEmployee").append("<tr>" + "<td>" + $("#newFirstName").val() + "</td>" + "<td>" + $("#newLastName").val() + "</td>" + "<td>" + $("#newId").val() + "</td>" + "<td>" + $("#newJobTitle").val() + "</td>" + "<td>" + $("#newAnnualSalary").val() + "</td>" + "<tr>");
          console.log("addNewEmployee");
+
     }
+
+    //function to feed in employee salaries
 
     //function totalBudget() {
     //    $("#totalMonthly").val()/12;
     //}
 
+    //function to clear feilds ?
+    //$('#myform')[0].reset();
+    //or
+    //document.getElementById("myform").reset();
 }
